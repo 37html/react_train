fe63fad7c04a84e80bc2c1c7861282861dedb684
Aleksey_B <borisov.ispu.@mail.ru>
2023-06-19 08:59:34 +0300

    добавлены посты

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-    // const [value, setValue] = useState('текст в инпуте');
 
+    const [posts,setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'},
+    ])
 
 
   return (
     <div className="App">
-        <Counter/>
-        <Counter/>
-        <Counter/>
-        <Counter/>
+        <form action="">
+            <input type="text" placeholder="Название поста"/>
+            <input type="text" placeholder="Описание поста"/>
+            <button>Создать о нас</button>
+        </form>
+        <PostList posts={posts} title='Посты про JS'/>
     </div>
   );
 }
