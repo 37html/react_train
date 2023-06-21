24015152a9320044710ed463a785d2ccd81d1a4e
Aleksey_B <borisov.ispu.@mail.ru>
2023-06-21 10:29:08 +0300

    добавлены управляемые компоненты инпуты

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import Counter from "./components/Counter";
 import './styles/App.css'
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import {logDOM} from "@testing-library/react";
 
 function App() {
 
@@ -14,22 +15,31 @@ function App() {
         {id: 3, title: 'Javascript 3', body: 'Description'},
     ])
 
-    const [title, setTitle] = useState('')
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
 
     const addNewPost = (e)=>{
         e.preventDefault()
+        console.log(title, body)
     }
 
   return (
     <div className="App">
         <form action="">
+            {/*Управляемый компонент*/}
             <MyInput
                 value={title}
                 type="text"
                 placeholder="Название поста"
                 onChange={e => setTitle( e.target.value)}
-                />
-            <MyInput type="text" placeholder="Описание поста"/>
+            />
+            {/*Управляемый компонент*/}
+            <MyInput
+                value={body}
+                type="text"
+                placeholder="Описание поста"
+                onChange={e => setBody(e.target.value)}
+            />
             <MyButton onClick={addNewPost}>Создать пост</MyButton>
         </form>
         <PostList posts={posts} title='Посты про JS'/>
