c59650ca2230ed6fb2cd4469d0472c2918161249
Aleksey_B <borisov.ispu.@mail.ru>
2023-06-28 11:05:00 +0300

    создание поста вынесено в модалку

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {logDOM} from "@testing-library/react";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
     ])
 
     const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false);
 
     const sortedPosts = useMemo(()=>{
         if(filter.sort){
@@ -33,17 +35,22 @@ function App() {
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     const removePost = (post) => {
        setPosts(posts.filter(p => p.id !== post.id))
     }
 
-
-
   return (
     <div className="App">
-        <PostForm create={createPost}/>
+        <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+            Создать пользователя
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost}/>
+        </MyModal>
+
         <hr style={{margin: '15px 0'}}/>
         <PostFilter
             filter={filter}
